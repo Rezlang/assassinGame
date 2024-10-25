ae3e4ce7933bda8d18e4f0395976afac554a3333
rahul_rathnam <rahulrathnam25@gmail.com>
2024-10-25 16:11:48 -0400

    made some changes to the create game page

--- frontend/pages/CreateGame.js
+++ frontend/pages/CreateGame.js
@@ -4,10 +4,8 @@ import { styles } from '../styles.js';
 import { useNavigation } from '@react-navigation/native';
 import { Picker } from '@react-native-picker/picker';
 
-RR
-this is my page now edit it so that it would work:
-const createGame = () =>{
-    const [selectedValue, setSelectedValue] = useState('option1');
+const CreateGame = () =>{
+    const [selectedValue, setSelectedValue] = useState('30');
 
     return (
         <View style={styles.container}>
@@ -25,48 +23,6 @@ const createGame = () =>{
       </Picker>
 
       {selectedValue === 'other' && (
-        <TextInput
-          style={styles.input}
-          onChangeText={setCustomValue}
-          value={customValue}
-          placeholder="Enter your own value"
-        />
-      )}
-    </View>
-    );
-  };
-
-
-Sure, I'll edit your component to make it work correctly. Here's the revised version:
-
-javascript
-
-Copy
-import React, { useState } from 'react';
-import { View, TextInput } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import styles from './path/to/your/style.js'; // Make sure this path is correct
-
-const CreateGame = () => {
-    const [roundTimeMins, setSelectedValue] = useState('30');
-
-    return (
-        <View style={styles.container}>
-            <Picker
-                selectedValue={selectedValue}
-                style={styles.picker}
-                onValueChange={(itemValue) => setSelectedValue(itemValue)}
-            >
-                <Picker.Item label="30 mins" value="30" />
-                <Picker.Item label="1 hour" value="60" />
-                <Picker.Item label="3 hours" value="180" />
-                <Picker.Item label="6 hours" value="360" />
-                <Picker.Item label="12 hours" value="720" />
-                <Picker.Item label="24 hours" value="1440" />
-                <Picker.Item label="Other" value="other" />
-            </Picker>
-
-            {selectedValue === 'other' && (
                 <TextInput
                 style={styles.input}
                 onChangeText={(text) => {
@@ -80,9 +36,8 @@ const CreateGame = () => {
                 keyboardType="numeric"
             />
         )}
-
-        </View>
+    </View>
     );
-};
+  };
 
 export default CreateGame;

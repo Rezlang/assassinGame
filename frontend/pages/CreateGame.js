0241f47493ff92a3d5c6225177cd3647a0af78fe
tmgross <tmgross8@gmail.com>
2024-11-05 16:28:11 -0500

    updated create game api call to send user information and cache returned game id

--- frontend/pages/CreateGame.js
+++ frontend/pages/CreateGame.js
@@ -1,87 +1,98 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Switch } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Switch, AsyncStorage } from 'react-native';
 import { styles, createGameStyles } from '../styles.js';
 import { useNavigation } from '@react-navigation/native';
 import { Picker } from '@react-native-picker/picker';
+import auth from '@react-native-firebase/auth';
 
 const CreateGame = () => {
-  const [roundLengthValueMins, setRoundLengthValue] = useState('30');
-  const [customRoundLengthValueMins, setCustomValue] = useState('');
-  const [killDistanceFeet, setKillDistance] = useState('5');
-  const [customKillDistanceFeet, setCustomKillDistance] = useState('');
-  const [shuffleTargets, setShuffleTargets] = useState(false);
-  const navigation = useNavigation();
+    const [roundLengthValueMins, setRoundLengthValue] = useState('30');
+    const [customRoundLengthValueMins, setCustomValue] = useState('');
+    const [killDistanceFeet, setKillDistance] = useState('5');
+    const [customKillDistanceFeet, setCustomKillDistance] = useState('');
+    const [shuffleTargets, setShuffleTargets] = useState(false);
+    const [userEmail, setUserEmail] = useState('');
+    const [userID, setUserID] = useState('');
+    const navigation = useNavigation();
 
-  return (
-      <ScrollView style={styles.homePageScrn}>
-          <Text style={styles.header}>Create Game</Text>
-          
-          <View style={createGameStyles.formContainer}>
-              {/* Round Length Section */}
-              <Text style={styles.subTitle}>Round Length</Text>
-              <View style={createGameStyles.pickerContainer}>
-                  <Picker
-                      selectedValue={roundLengthValueMins}
-                      style={createGameStyles.picker}
-                      dropdownIconColor="white"
-                      onValueChange={(itemValue) => setRoundLengthValue(itemValue)}
-                  >
-                      <Picker.Item label="30 mins" value="30" color="white" />
-                      <Picker.Item label="1 hour" value="60" color="white" />
-                      <Picker.Item label="3 hours" value="180" color="white" />
-                      <Picker.Item label="6 hours" value="360" color="white" />
-                      <Picker.Item label="12 hours" value="720" color="white" />
-                      <Picker.Item label="Other" value="other" color="white" />
-                  </Picker>
-              </View>
+    useEffect(() => {
+        const user = auth().currentUser;
+        if (user) {
+            setUserEmail(user.email);
+            setUserID(user.uid)
+        }
+    }, []);
 
-              {roundLengthValueMins === 'other' && (
-                  <TextInput
-                      style={styles.input}
-                      onChangeText={(text) => {
-                          const days = parseFloat(text) || 0;
-                          const minutes = days * 1440;
-                          setCustomValue(text);
-                          setRoundLengthValue(minutes.toString());
-                      }}
-                      value={customRoundLengthValueMins}
-                      placeholder="Enter number of days"
-                      placeholderTextColor="#666"
-                      keyboardType="numeric"
-                  />
-              )}
+    return (
+        <ScrollView style={styles.homePageScrn}>
+            <Text style={styles.header}>Create Game</Text>
 
-              {/* Kill Distance Section */}
-              <Text style={styles.subTitle}>Kill Distance (feet)</Text>
-              <View style={createGameStyles.pickerContainer}>
-                  <Picker
-                      selectedValue={killDistanceFeet}
-                      style={createGameStyles.picker}
-                      dropdownIconColor="white"
-                      onValueChange={(itemValue) => setKillDistance(itemValue)}
-                  >
-                      <Picker.Item label="5 feet" value="5" color="white" />
-                      <Picker.Item label="10 feet" value="10" color="white" />
-                      <Picker.Item label="25 feet" value="25" color="white" />
-                      <Picker.Item label="50 feet" value="50" color="white" />
-                      <Picker.Item label="100 feet" value="100" color="white" />
-                      <Picker.Item label="Other" value="other" color="white" />
-                  </Picker>
-              </View>
+            <View style={createGameStyles.formContainer}>
+                {/* Round Length Section */}
+                <Text style={styles.subTitle}>Round Length</Text>
+                <View style={createGameStyles.pickerContainer}>
+                    <Picker
+                        selectedValue={roundLengthValueMins}
+                        style={createGameStyles.picker}
+                        dropdownIconColor="white"
+                        onValueChange={(itemValue) => setRoundLengthValue(itemValue)}
+                    >
+                        <Picker.Item label="30 mins" value="30" color="white" />
+                        <Picker.Item label="1 hour" value="60" color="white" />
+                        <Picker.Item label="3 hours" value="180" color="white" />
+                        <Picker.Item label="6 hours" value="360" color="white" />
+                        <Picker.Item label="12 hours" value="720" color="white" />
+                        <Picker.Item label="Other" value="other" color="white" />
+                    </Picker>
+                </View>
 
-              {killDistanceFeet === 'other' && (
-                  <TextInput
-                      style={styles.input}
-                      onChangeText={(text) => {
-                          setCustomKillDistance(text);
-                          if (text) setKillDistance(text);
-                      }}
-                      value={customKillDistanceFeet}
-                      placeholder="Enter distance in feet"
-                      placeholderTextColor="#666"
-                      keyboardType="numeric"
-                  />
-              )}
+                {roundLengthValueMins === 'other' && (
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(text) => {
+                            const days = parseFloat(text) || 0;
+                            const minutes = days * 1440;
+                            setCustomValue(text);
+                            setRoundLengthValue(minutes.toString());
+                        }}
+                        value={customRoundLengthValueMins}
+                        placeholder="Enter number of days"
+                        placeholderTextColor="#666"
+                        keyboardType="numeric"
+                    />
+                )}
+
+                {/* Kill Distance Section */}
+                <Text style={styles.subTitle}>Kill Distance (feet)</Text>
+                <View style={createGameStyles.pickerContainer}>
+                    <Picker
+                        selectedValue={killDistanceFeet}
+                        style={createGameStyles.picker}
+                        dropdownIconColor="white"
+                        onValueChange={(itemValue) => setKillDistance(itemValue)}
+                    >
+                        <Picker.Item label="5 feet" value="5" color="white" />
+                        <Picker.Item label="10 feet" value="10" color="white" />
+                        <Picker.Item label="25 feet" value="25" color="white" />
+                        <Picker.Item label="50 feet" value="50" color="white" />
+                        <Picker.Item label="100 feet" value="100" color="white" />
+                        <Picker.Item label="Other" value="other" color="white" />
+                    </Picker>
+                </View>
+
+                {killDistanceFeet === 'other' && (
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(text) => {
+                            setCustomKillDistance(text);
+                            if (text) setKillDistance(text);
+                        }}
+                        value={customKillDistanceFeet}
+                        placeholder="Enter distance in feet"
+                        placeholderTextColor="#666"
+                        keyboardType="numeric"
+                    />
+                )}
 
                 {/* Shuffle Targets Section */}
                 <TouchableOpacity
@@ -115,8 +126,8 @@ const CreateGame = () => {
                     //TODO: pass additional game settings through request and set actual owner name and id
                     const url = 'http://128.113.126.109/create_game';
                     const requestData = {
-                        owner_name: 'placeholder_name',
-                        owner_id: 'placeholder_id'
+                        owner_name: userEmail,
+                        owner_id: userID
                     };
 
                     try {
@@ -129,8 +140,17 @@ const CreateGame = () => {
                         });
 
                         const result = await response.json();
+                        
                         if (response.ok) {
                             Alert.alert('Game Created', `Game ID: ${result.game_id}`);
+                            try {
+                                await AsyncStorage.setItem(
+                                    'game_id',
+                                    result.game_id,
+                                );
+                            } catch (error) {
+                                Alert.alert('Error saving Game ID');
+                            }
                         } else {
                             Alert.alert('Error Creating Game', result.error);
                         }
@@ -144,7 +164,7 @@ const CreateGame = () => {
                         killDistanceFeet: killDistanceFeet,
                         shuffleTargets: shuffleTargets
                     });
-                
+
                 }}
             >
                 <Text style={styles.buttonText}>Create Game</Text>
